ce21df47d13805bb3da3c31e4ad05ac5c174effd
Мария <oms1999@yandex.ru>
2024-05-15 03:03:39 +0300

    Add users app

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(verbose_name='почта', unique=True)
+    phone = PhoneNumberField(region='RU', verbose_name='номер телефона', **NULLABLE)
+    town = models.CharField(max_length=50, verbose_name='город', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
