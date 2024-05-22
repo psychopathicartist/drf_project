3f2c5f1fe849d937f10ac3e45d54be0aff5b567b
Мария <oms1999@yandex.ru>
2024-05-23 01:26:34 +0300

    Modify materials app (add author and perms)

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
@@ -7,6 +8,7 @@ class Course(models.Model):
     name = models.CharField(max_length=150, verbose_name='название')
     preview = models.ImageField(upload_to='courses/', verbose_name='превью', **NULLABLE)
     description = models.TextField(verbose_name='описание')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='автор')
 
     def __str__(self):
         return f'{self.name}'
@@ -22,6 +24,7 @@ class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
     preview = models.ImageField(upload_to='lessons/', verbose_name='превью', **NULLABLE)
     video = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='автор')
 
     def __str__(self):
         return f'{self.name}'
